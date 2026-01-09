11c04e5c6a9bb704f7ba33ae9f7f81ff65cef0a5
rajathithan <rajathithan@gmail.com>
2026-01-09 21:20:33 +0530

    updates

--- basics_13_functions/main.go
+++ basics_13_functions/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // functions are first-class citizens in Go
 // you can pass them as arguments and return them from other functions
@@ -22,15 +25,58 @@ func getOperation() func(int, int) int {
 	return add
 }
 
+// functions returning multiple return of same types
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+// functions returning multiple return of different types
+func combine(a int, b string) (string, int) {
+	return b, a
+}
+
+// functions returning a type & a error type
+func compare(a, b float64) (float64, error) {
+	if a > b {
+		return a, nil
+	} else if b > a {
+		return b, nil
+	} else {
+		//return 0, fmt.Errorf("both values are equal")
+		return 0, errors.New("both values are equal")
+	}
+}
+
 func main() {
+	// calling the add function
 	ans := add(3, 5)
 	fmt.Println("The sum is:", ans)
 
+	// passing add function as argument to operate function
 	result := operate(10, 20, add)
 	fmt.Println("The result of operation is:", result)
 
+	// getting function from another function
 	op := getOperation()
 	// Here the op variable holds the add function
 	finalResult := op(7, 8)
 	fmt.Println("The final result is:", finalResult)
+
+	// multiple return values of same types
+	a, b := "hello", "world"
+	x, y := swap(a, b)
+	fmt.Println("Swapped values:", x, y)
+
+	// multiple return values of different types
+	str, num := combine(42, "The answer is")
+	fmt.Println(str, num)
+
+	// function returning a type & an error type
+	output, err := compare(10.232323, 2.23434323232)
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Println("The output value is :", output)
+	}
+
 }
