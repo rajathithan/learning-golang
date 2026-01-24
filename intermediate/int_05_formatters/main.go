a026bcc674fabbb5681aafea94b0d12aac58d1b9
rajathithan <rajathithan@gmail.com>
2026-01-24 22:41:21 +0530

    updates

--- intermediate/int_05_formatters/main.go
+++ intermediate/int_05_formatters/main.go
@@ -17,4 +17,19 @@ func main() {
 	fmt.Printf("Go-syntax representation: %#v\n", floatNum)
 	fmt.Printf("Type of variable: %T\n", floatNum)
 	fmt.Printf("Literal percent sign: %v %%\n", floatNum)
+
+	// Formatting integers in different bases
+	intNum := 255
+	
+	fmt.Printf("Decimal: %d\n", intNum)
+	fmt.Printf("Decimal with sign: %+d\n", intNum)
+	fmt.Printf("Decimal right  : %4d\n", intNum)
+	fmt.Printf("Decimal left   : %-4d\n", intNum)
+	fmt.Printf("Decimal with zeroes padded : %04d\n", intNum)
+	fmt.Printf("Binary: %b\n", intNum)
+	fmt.Printf("Hexadecimal: %x\n", intNum)
+	fmt.Printf("Hexadecimal with uppercase: %X\n", intNum)
+	fmt.Printf("Hexadecimal with prefix: %#x\n", intNum)
+	fmt.Printf("Octal: %o\n", intNum)
+	fmt.Printf("Octal with prefix: %#o\n", intNum)
 }
