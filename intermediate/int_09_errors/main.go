292ddbe45cf1bd3b9431495c0492f7a4f2fc6fbd
rajathithan <rajathithan@gmail.com>
2026-02-09 12:27:02 +0530

    updates

--- intermediate/int_09_errors/main.go
+++ intermediate/int_09_errors/main.go
@@ -26,7 +26,7 @@ func sqrt(value float64) (float64, error) {
 		// Implementing error using a custom error type
 		//return 0, newCustomError("cannot compute square root of a negative number")
 
-		// Implementing error wrapping with %w (Go 1.13+)
+		// Implementing using Errorf function & error wrapping with %w (Go 1.13+)
 		//baseErr := errors.New("math domain error")
 		//return 0, fmt.Errorf("cannot compute square root of %v: %w", value, baseErr)
 	}
